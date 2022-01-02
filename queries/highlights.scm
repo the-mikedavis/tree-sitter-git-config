50ffe7af1e812f8cde62649f4d1aa957c1fed59b
Michael Davis <mcarsondavis@gmail.com>
2022-01-02 11:00:07 -0600

    setup highlight queries

---
+++ queries/highlights.scm
@@ -0,0 +1,15 @@
+(section_name) @tag
+(variable (name) @property)
+[(true) (false)] @constant.builtin
+(integer) @number
+[(string) (subsection_name)] @string
+
+[
+  "["
+  "]"
+  "\""
+] @punctuation.bracket
+
+"=" @punctuation.delimiter
+
+(comment) @comment
